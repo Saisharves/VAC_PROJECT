40cddff6c903773be50d289cb16d42cf49c0ae00
SAI SHARVESH R <92215485+Saisharves@users.noreply.github.com>
2024-02-09 13:58:39 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-await mongoose.connect(process.env.MONGO_URL || MONGO_URI);
+await mongoose.connect(process.env.MONGO_URL || "mongodb+srv://sibi:MeuYCbR6iEUnptJi @cluster0.3az1wjg.mongodb.net/?retryWrites=true&w=majority");
     console.log("connected to MONGODB");
   } catch (err) {
     throw err;
